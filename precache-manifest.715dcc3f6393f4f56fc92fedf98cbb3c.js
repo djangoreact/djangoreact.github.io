bd1088dcd59bb018ccdfcb59b0b67eb7e3397ba8
DILIP KUMAR SHARMA <krdilip276@gmail.com>
2019-07-23 05:33:12 +0530

    Add files via upload

---
+++ precache-manifest.715dcc3f6393f4f56fc92fedf98cbb3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b14b3e1f9d6ad9de61171312ed5b430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "448707d73478acc71161",
+    "url": "/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "85145e80bf5636ef5a00",
+    "url": "/static/css/main.bb36bedc.chunk.css"
+  },
+  {
+    "revision": "448707d73478acc71161",
+    "url": "/static/js/2.a3d1097f.chunk.js"
+  },
+  {
+    "revision": "85145e80bf5636ef5a00",
+    "url": "/static/js/main.720d9770.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "132500c82fae24df0233d9766ee6f777",
+    "url": "/static/media/espnlogo.132500c8.jpg"
+  },
+  {
+    "revision": "370e88cc4cdec140000c03c365729a79",
+    "url": "/static/media/logosmall.370e88cc.png"
+  }
+]);
